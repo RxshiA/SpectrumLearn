efab331877125f3187797433279388df3ff48828
RxshiA <rashiamarasiri@gmail.com>
2024-11-27 22:14:27 +0530

    initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.svg$': 'jest-transform-stub',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFiles: ['<rootDir>/jest/setup.js'],
 };
